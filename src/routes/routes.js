67cfac716970dabe11198373a9a68d8b6574a46a
Thien Vo <vohuythien@utexas.edu>
2019-01-27 13:25:08 -0600

    main api calls finished

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,9 +3,12 @@ module.exports = (app) => {
     const util = require('../util/utils.js');
     const request = require('request');
     const redirectURI = process.env.REDIRECT_URI || 'http://localhost:9090/callback';
-    const client_id = process.env.SPOTIFY_CLIENT_ID || null;
-    const client_secret = process.env.SPOTIFY_CLIENT_SECRET || null;
+    const clientID = process.env.SPOTIFY_CLIENT_ID || null;
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET || null;
     const stateKey = 'spotify_auth_state';
+    let accessToken = null;
+    let refreshToken = null;
+    let expires = null;
 
     //User is asked to authorize access with predefined scopes
     //User then redirected to 'redirectURI'
@@ -17,7 +20,7 @@ module.exports = (app) => {
         res.redirect('https://accounts.spotify.com/authorize?' + 
             querystring.stringify({
                 response_type: 'code',
-                client_id: client_id,
+                client_id: clientID,
                 redirect_uri: redirectURI,
                 state: state,
                 scope: scope
@@ -33,7 +36,7 @@ module.exports = (app) => {
         const state = req.query.state || null;
         const storedState = req.cookies ? req.cookies[stateKey] : null;
         if ( state === null || state !== storedState) {
-            res.json({
+            res.status(400).json({
                 error: 'state_mismatch'
             });
         } else {
@@ -46,22 +49,24 @@ module.exports = (app) => {
                     redirect_uri: redirectURI
                 },
                 headers: {
-                    'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
+                    'Authorization': 'Basic ' + (Buffer.from(clientID + ':' + clientSecret).toString('base64'))
                 },
                 json: true
             };
             request.post(authOptions, (error, response, body) => {
                 if (!error && response.statusCode === 200 ) {
                     //TODO: REDIRECT TO CLIENT
-                    res.json({
-                        access_token: body.access_token,
-                        refresh_token: body.refresh_token,
+                    //TODO: REMOVE JSON HERE
+                    res.status(200).json({
+                        body: body
                     });
+                    expires = body.expires_in;
+                    accessToken = body.access_token;
+                    refreshToken = body.refresh_token;
                 } else {
-                    res.json({
-                        error: error,
-                        query: req.query,
-                        response: response
+                    res.status(response.statusCode).json({
+                        error: body.error,
+                        errorDescription: body.error_description
                     });
                 }
             });
@@ -71,12 +76,12 @@ module.exports = (app) => {
 
     //Turn this into function and call whenever refresh is required?
     app.get('/refresh_token', (req, res) => {
-        let refresh_token = req.query.refresh_token;
+        let refreshToken = req.query.refresh_token;
         let authOptions = {
-            headers: { 'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64')},
+            headers: { 'Authorization': 'Basic ' + Buffer.from(clientID + ':' + clientSecret).toString('base64')},
             form: {
                 grant_type: 'refresh_token',
-                refresh_token: refresh_token
+                refresh_token: refreshToken
             },
             json: true
         };
@@ -84,7 +89,7 @@ module.exports = (app) => {
         request.post(authOptions, (error, response, body) => {
             if (!error && response.statusCode === 200) {
                 //TODO: Save refreshtoken
-                let refreshedToken = body.refresh_token;
+                refreshToken = body.refresh_token;
             } else {
                 //TODO: add invalid request redirection
                 res.redirect('/#', querystring.stringify({
@@ -94,16 +99,101 @@ module.exports = (app) => {
         });
     });
 
-    app.get('/home', (req, res) => {
-        res.send('<html><body>HOME</body></html>');
+    app.get('/search/song', (req, res) => {
+        let query = req.query.q;
+        if (accessToken && refreshToken && query) {
+            let queryParams = {
+                url: 'https://api.spotify.com/v1/search',
+                qs: {
+                    q: query,
+                    type: 'track'
+                },
+                headers: { 'Authorization' : 'Bearer ' + accessToken },
+                json: true
+            };
+            request.get(queryParams, (error, response, body) => {
+                if ( !error && response.statusCode === 200) {
+                    let data = body.tracks.items.map((item) => {
+                        let artists = item.artists.map((artist) => {return artist.name});
+                        return {
+                            name: item.name,
+                            album_art: item.album.images[1],
+                            artists: artists
+                        };
+                    });
+                    res.json(data);
+                    //process data body.tracks.items
+                    //items array
+                        //item.name (Track Name)
+                        //item.album.images => typically 3 sizes | choose best one
+                        //item.artists 0 == main, ft. => >0
+                } else {
+                    res.status(400).json({
+                        error: error ? error : 'invalid_request'
+                    });
+                }
+            });
+        } else {
+            res.status(400).json({
+                error: query ? 'invalid_token' : 'invalid_query'
+            });
+        }
     });
 
-    app.get('/song/:query', (req, res) => {
+    app.get('/search/lyric', (req, res) => {
+        let name = req.query.name;
+        let artist = req.query.artist; //primary artist
+        console.log(`NAME: ${name} ARTIST: ${artist}`);
+        if (name && artist) {
+            let params = {
+                url: 'https://api.genius.com/search',
+                headers: { 'Authorization' : 'Bearer ' + process.env.GENIUS_CLIENT_ACCESS_TOKEN },
+                qs: {
+                    q: `${name} ${artist}`
+                },
+                json: true
+            };
+            //Request lyrics from Genius
+            //Filter first 3 and find if title and partial artist match
+            request.get(params, (error, response, body) => {
+                let lyrics = [];
+                if (!error && response.statusCode === 200 && body) {
+                    let found = body.response.hits.slice(0, 3).find((hit) => {
+                        return hit.result.title.toUpperCase() === name.toUpperCase() &&
+                        hit.result.primary_artist.name.toUpperCase().includes(artist.toUpperCase());
+                    });
+                    if (found) {
+                        request.get({url: found.result.url}, (pageError, pageResponse, page)  => {
+                            if (!pageError && pageResponse.statusCode === 200) {
+                                let cheerio = require('cheerio');
+                                let html = cheerio.load(page);
+                                //TODO: AWAIT
+                                lyrics.push(html('div.lyrics p').text());
+                            }
+                        });
+                    }
+                    res.status(200).json({lyrics});
+                }
+            });
+            //get song link via api
+            //use python-shell to run python script to scrape lyrics
+            //return lyrics array => {}
+            //Typically the first hit => check first 3
+            //hits => (hit) => {
+                //hit.title => verify title to query.name
+                //hit.primary_artist.name => verify artist matches at least one
+                //hit.url => parse lyrics
+            //}
 
+            //if found song => use script to parse lyrics
+            //Return lyrics array with objects containing "Original", and other languages if available
 
-
+        } else {
+            res.status(400).json({
+                error: 'invalid_artist or invalid_name'
+            });
+        }
 
 
     });
-
 };
