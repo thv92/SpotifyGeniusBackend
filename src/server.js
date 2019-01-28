5c5894b262b64478ae2bebe9fcefae997b6eb89f
Thien Vo <vohuythien@utexas.edu>
2019-01-27 19:46:35 -0600

    Had to move routes import down in order to get everything in before routes was imported

--- src/server.js
+++ src/server.js
@@ -1,20 +1,17 @@
 const path = require('path');
+require('dotenv').config({
+    path: path.resolve(__dirname, '../.env')
+});
 const express = require('express');
 const cors = require('cors');
 const cookieparser = require('cookie-parser');
 const app = express();
-const routes = require('./routes/routes');
-
-require('dotenv').config({
-    path: path.resolve(__dirname, '../.env')
-});
-
 const port = process.env.PORT || 9090;
 
 app.use(cors())
-   .use(cookieparser());
-
+.use(cookieparser());
 //configure routes
+const routes = require('./routes/routes');
 routes(app);
 
 console.log('Listening on port: ' + port);
