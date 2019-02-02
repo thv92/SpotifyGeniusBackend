4140eafeacd97975b97041f8203f487eeed441cf
Thien Vo <vohuythien@utexas.edu>
2019-02-01 22:21:04 -0600

    switched name and artist for genius request

--- src/controllers/geniusController.js
+++ src/controllers/geniusController.js
@@ -1,23 +1,20 @@
 const geniusService = require('../services/geniusService');
 const scrapeService = require('../services/scrapeService');
 const searchLyric = (req, res) => {
-    console.log('GET ' + req.path);
     const name = req.query.name;
     const artist = req.query.artist;
-
     if (name && artist) {
         const params = {
             url: 'https://api.genius.com/search',
             headers: { 'Authorization' : 'Bearer ' + process.env.GENIUS_CLIENT_ACCESS_TOKEN },
             qs: {
-                q: `${name} ${artist}`
+                q: `${artist} ${name}`
             },
             json: true
         };
-
         geniusService.requestLyrics(params).then((hits) => {
             let found = hits.slice(0, 3).find((hit) => {
-                return hit.result.title.toUpperCase() === name.toUpperCase()
+                return hit.result.title.toUpperCase().includes(name.toUpperCase())
                     && hit.result.primary_artist.name.toUpperCase().includes(artist.toUpperCase());
             });
             return found;
